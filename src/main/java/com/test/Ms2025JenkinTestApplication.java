8e0c6b46b7d53a82eb2be24e103d47e39816f714
GopalWaghule <gopalwaghule1997@gmail.com>
2025-02-23 20:00:59 +0530

    initialcommit

---
+++ src/main/java/com/test/Ms2025JenkinTestApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms2025JenkinTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms2025JenkinTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/Ms2025JenkinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms2025JenkinTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
